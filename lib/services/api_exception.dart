9bb307e6c56d80e80019fc2cf70a6071dfb6f6e6
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-09 23:19:51 +0300

    Add ApiException class

---
+++ lib/services/api_exception.dart
@@ -0,0 +1,15 @@
+class ApiException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  ApiException(this.message, [this.statusCode]);
+
+  @override
+  String toString() {
+    if (statusCode != null) {
+      return 'ApiException ($statusCode): $message';
+    }
+
+    return 'ApiException: $message';
+  }
+}
